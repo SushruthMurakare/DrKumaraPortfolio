873768bf6e616d3abaf26c50ba323436e208e8c4
SushruthMurakare <sushruthraomurakare@gmail.com>
2024-09-01 10:47:22 -0600

    added comma

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -7,10 +7,10 @@ const endpoints = {
   education: 'profile/education.json',
   experiences: 'profile/experiences.json',
   projects: 'profile/projects.json',
-  awards: 'profile/awards.json',  
+  awards: 'profile/awards.json',
   grants: 'profile/grants.json',
   patents: 'profile/patents.json',
-  publications: 'profile/publications.json'
+  publications: 'profile/publications.json',
 };
 
 export default endpoints;
